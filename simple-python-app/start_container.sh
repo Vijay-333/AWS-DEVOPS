45e081c6f95a3f2243bd96e7e5d09e02e12fa69b
demo-user <vijayvelraja333recovery@gmail.com>
2024-06-12 23:59:40 +0530

    Added docker statements on start container

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull vijayvelraja/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 vijayvelraja/simple-python-flask-app
